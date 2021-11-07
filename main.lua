c9eb2589b4d813b403e1a7586337e9a5a3460fa8
Gustavo Favaro <54089418+BlakeZero@users.noreply.github.com>
2021-11-07 18:53:32 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,38 @@
 userInterface = require("interface")
-userInterface.launcher()
-userInterface.game()
+t_utils = require("triviaUtils")
+require("wx")
 
-wx.wxGetApp():MainLoop()
+function launcher()
+    while true do
+        local nickname, addr = userInterface.launcher()
+        local ip, port = t_utils.separadorEndereco(addr, ':')
+        local tcp = t_utils.socketConnection(ip, port)
+
+        if tcp == 0 then
+            userInterface.erroServer()
+        else
+            tcp:send(nickname)
+            local s, status, partial = tcp:receive()
+            if s == 'NICKNAME ERR' then userInterface.erroNickname()
+            else break end
+        end
+        if not tcp then return tcp end
+    end
+end
+
+function ingame(tcp)
+    while true do
+        local s, status, partial = tcp:receive()
+        userInterface.game(s)
+        if status == 'closed' then break end
+    end
+end
+
+function main()
+    local tcp = launcher()
+    if not tcp then return nil end
+    ingame(tcp)
+    wx.wxGetApp():MainLoop()
+end
+
+main()
