86cc059f70bf3af2251972011ba5f0aec86a8e58
Fabricio Flávio <52521053+Fabricio-Martins@users.noreply.github.com>
2021-12-10 20:20:17 -0300

    Update servidor.py

--- Versão GTK/servidor.py
+++ Versão GTK/servidor.py
@@ -1,89 +1,50 @@
 import socket
 import threading
 
-# Endereço IP do servidor
-HOST = '127.0.0.1' # conexão com localhost
-PORT = 50000
+host = '127.0.0.1'
+port = 5555
 
-# Definição da classe jogador
-class Jogador:
-    def __init__(self, nickname, endereco):
-        self.nickname = nickname # nome do jogador
-        self.endereco = endereco # endereço de ip
-        self.status = '' # status ingame (jogador da vez, jogador acertou a resposta, etc)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 
-    def getNickname(self):
-        return self.nickname
 
-    def getEndereco(self):
-        return self.endereco
+jogadores = [] # 0 -> nickname / 1 -> conn / 2 -> status
 
-    def getStatus(self):
-        return self.status
+def broadcast(message):
+    for jogador in jogadores:
+        jogador[1].send(message)
 
-    def setNickname(self, nickname):
-        self.nickname = nickname
-
-    def setEndereco(self, endereco):
-        self.endereco = endereco
-    
-    def setStatus(self, status):
-        self.status = status
-
-jogadores = []
-
-# Função que manda uma mesma mensagem para todos os clientes
-def broadcast(msg):
-    for i in range(len(jogadores)):
-        endereco = jogadores[i].getEndereco()
-        endereco.send(str.encode(msg))
-
-# Função que gerencia a interação do cliente com o servidor
 def handle(conn):
-    while True:    
-        try:
-            # interação do cliente com o servidor
-            msg = conn.recv(1024)
-            for i in range(len(jogadores)):
-                if conn in jogadores[i].getNickname(): origem = jogadores.getNickname()                        
-            broadcast(f'{origem}: {msg}')
-        except:
-            # jogador saindo do jogo encerra a conexão com o cliente
-            for i in range(len(jogadores)):
-                if conn == jogadores[i].getEndereco(): jogadorSaindo = jogadores[i]
-            broadcast(f'{jogadorSaindo.getNickname()} saiu da sala.')
-            jogadores.remove(jogadorSaindo)
-            conn.close()
-            break
-        
-
-def main():
-    # Abrindo o servidor
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((HOST, PORT))
-    sock.listen(10)
-    print('Servidor aberto\n\n')
-
-    # Loop do servidor
     while True:
-        # Estabelece conexão com o cliente e o registra no servidor
-        conn, addr = sock.accept()
-        ip, prt = addr
-        print(f'Usuário de IP {ip}:{prt} conectou-se ao servidor!')
-        
-        nickname = conn.recv(1024).decode()
-        if jogadores:
-            for i in range(len(jogadores)):
-                if nickname in jogadores[i].getNickname(): 
-                    conn.send(str.encode('NICKNAME ERR')) # envia o código de erro de apelido repetido pro cliente
+        try:
+            message = conn.recv(1024)
+            broadcast(message)
+        except:
+            for jogador in jogadores:
+                if jogador[1] == conn:
                     conn.close()
-             
-        jogadores.append(Jogador(nickname, conn))
-        broadcast(f'{nickname} entrou na sala!')
+                    broadcast('{} saiu da sessao.#CHAT_TYPE'.format(jogador[0]).encode('utf-8'))
+                    jogadores.remove(jogador)
+                    break
+            break
 
-        # Threading
-        thread = threading.Thread(target = handle, args = (conn,))
-        thread.start()
+def receive():
+    while True:
+        conn, addr = server.accept()
+        ipClient, portClient = addr
+        print(f"Connected with {ipClient}:{portClient}", end = ' ')
 
+        # Request And Store Nickname
+        nickname = conn.recv(1024).decode('utf-8')
+        jogadores.append([nickname, conn])
+        
+        # Print And Broadcast Nickname
+        print('as "{}"'.format(nickname))
+        broadcast("{} entrou na sessao!#CHAT_TYPE".format(nickname).encode('utf-8'))
+        broadcast("{}#NICK_TYPE".format(nickname).encode('utf-8'))
 
-#main() # Execução do servidor
+        thread = threading.Thread(target=handle, args=(conn,))
+        thread.start()   
+
+receive()
